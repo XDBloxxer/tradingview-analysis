511df3fa96d584e837d75c47572e70a8ace79ad0
XDBloxxer <xdbloxxer@gmail.com>
2026-02-05 18:40:36 +0200

    Update backtesting__init__.py

--- backtesting/backtesting__init__.py
+++ backtesting/backtesting__init__.py
@@ -1,13 +1,1 @@
-"""
-Backtesting module for strategy testing
-"""
 
-__version__ = "1.0.0"
-
-from .strategy_backtester import StrategyBacktester
-from .backtest_supabase_client import BacktestSupabaseClient
-
-__all__ = [
-    'StrategyBacktester',
-    'BacktestSupabaseClient'
-]
