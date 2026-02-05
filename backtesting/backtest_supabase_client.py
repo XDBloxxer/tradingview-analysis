c3544808adad370ee4d7930dc9797202dca1e949
XDBloxxer <xdbloxxer@gmail.com>
2026-02-05 13:01:14 +0200

    Update backtest_supabase_client.py

--- backtesting/backtest_supabase_client.py
+++ backtesting/backtest_supabase_client.py
@@ -52,7 +52,7 @@ class BacktestSupabaseClient:
         """Sanitize value for Supabase"""
         if value is None:
             return None
-        if pd.a.all(value):
+        if pd.all(value):
             return None
         if isinstance(value, np.integer):
             return int(value)
